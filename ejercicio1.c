81b4393ad3b1e3f99e4509b00fcead43187cf270
ArgerichFacu <mfacundo.argerich@gmail.com>
2026-08-08 14:32:49 -0300

    test

---
+++ ejercicio1.c
@@ -0,0 +1,15 @@
+/*
+Ejercicio 1.1 – Hola Mundo
+Escribir un programa que imprima "Hola, mundo!" en la pantalla.
+Este ejercicio nos permitirá verificar que el entorno de programación funciona.
+-----------------
+Nombre y Apellido
+Usuario Github
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola, mundo!\n");
+    return 0;
+}
