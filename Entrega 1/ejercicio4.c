5234fe33840725a8173d67fa0450dd2d27f38054
ArgerichFacu <mfacundo.argerich@gmail.com>
2026-08-08 16:13:38 -0300

    e1,2,3,4

---
+++ Entrega 1/ejercicio4.c
@@ -0,0 +1,15 @@
+/*
+Ejercicio 4 – Invertir un Número 
+Implementar una función que tome un número entero y devuelva otro número con los dígitos en orden inverso.
+-----------------
+Facundo Argerich
+Github: ArgerichFacu
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+
+    return 0;
+}
